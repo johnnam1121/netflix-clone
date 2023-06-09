ae98aabda4b813caf9b08a3ba7ac97a162e8ca32
Johnnam1121 <johnnam93@gmail.com>
2023-06-09 17:34:25 -0500

    taking a break

---
+++ pages/auth.tsx
@@ -0,0 +1,90 @@
+'use client'
+import { useCallback, useState } from "react";
+import Input from "../components/Input";
+import axios from "axios";
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router'
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
+const Auth = () => {
+  const router = useRouter();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [variant, setVariant] = useState('login');
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+  }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/'
+      });
+      router.push('/');
+    } catch (error) {
+      console.log(error)
+    }
+  }, [email, password, router])
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('./api/register', {
+        email,
+        name,
+        password
+      });
+      login();
+    } catch (error) {
+      console.log(error)
+    }
+  }, [email, name, password]);
+
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === 'login' ? 'Sign in' : 'Create a new account'}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === 'register' && (
+                <Input onChange={(e: any) => setName(e.target.value)} label='Username' id='name' type='name' value={name} />
+              )}
+              <Input onChange={(e: any) => setEmail(e.target.value)} label='Email' id='email' type='email' value={email} />
+              <Input onChange={(e: any) => setPassword(e.target.value)} label='Password' id='password' type='password' value={password} />
+            </div>
+            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === 'login' ? 'Login' : 'Sign up'}
+            </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FcGoogle size={32} />
+              </div>
+              <div onClick={() => signIn('github', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FaGithub size={32} />
+              </div>
+            </div>            <p className="text-neutral-500 mt-12">
+              {variant === 'login' ? 'New user?' : 'Already have an account?'}
+              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                {variant === 'login' ? 'Create a new account' : 'Sign in'}
+              </span>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
